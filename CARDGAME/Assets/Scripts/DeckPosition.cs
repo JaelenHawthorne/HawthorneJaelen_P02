b5ded59524b4ad441382893f07cd74ae9a3a1215
Jaelen Hawthorne <jeh160230@utdallas.edu>
2020-10-22 19:42:05 -0400

    Project02A basics
    projects basics complete. (card system, card UI)

---
+++ CARDGAME/Assets/Scripts/DeckPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class DeckPosition : MonoBehaviour
+public enum DeckPosition
+{
+    Top,
+    Middle,
+    Bottom
+}
---
+++ CARDGAME/Assets/Scripts/TargetContoller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetContoller : MonoBehaviour
+{
+    //TODO build more structured connection
+    public static ITargetable CurrentTarget;
+    // interface dont stabalize, so no need class refrence
+    [SerializeField] Creature _objectToTarget = null;
+
+    private void Update()
+    {
+        // target the object when '1' is pressed
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // target the object, if it is targetable
+            ITargetable possibleTarget = _objectToTarget.GetComponent<ITargetable>();
+            if(possibleTarget != null)
+            {
+                Debug.Log("New target Acquired!");
+                CurrentTarget = possibleTarget;
+                _objectToTarget.Target();
+            }
+
+        }
+    }
+}
